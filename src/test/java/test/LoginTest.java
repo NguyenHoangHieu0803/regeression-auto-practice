7aa53d58638003b8de3e7246045bb9c0c5c3d21e
NguyenHoangHieu0803 <hieu.contact.8321@gmail.com>
2024-06-09 19:19:56 +0700

    test

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -1,4 +1,24 @@
 package test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+
+    //login sucessfully with standard user https://www.saucedemo.com/
+
+    @Test
+    void loginSuccessStandardUser () {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.xpath("//input[@placeholder=\"Username\"]")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@placeholder=\"Password\"]")).sendKeys("secret_sauce");
+
+        driver.findElement(By.xpath("//input[@type=\"submit\"]")).click();
+        Assert.assertTrue(driver.getCurrentUrl().equals("https://www.saucedemo.com/inventory.html"));
+    }
 }
